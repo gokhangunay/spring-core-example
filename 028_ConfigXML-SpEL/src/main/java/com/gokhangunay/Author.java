cadedfd263b55b3a970c29a90a6c656c35edeb5f
Gökhan Günay <gokhan.gunay@outlook.com>
2016-04-28 20:58:38 +0300

    Spring Core Example

---
+++ 028_ConfigXML-SpEL/src/main/java/com/gokhangunay/Author.java
@@ -0,0 +1,54 @@
+package com.gokhangunay;
+
+public class Author {
+	
+	private Book book;
+	
+	private String authorName;
+	
+	private String authorSurname;
+	
+	private String authorBookName;
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getAuthorSurname() {
+		return authorSurname;
+	}
+
+	public void setAuthorSurname(String authorSurname) {
+		this.authorSurname = authorSurname;
+	}
+
+	public String getAuthorBookName() {
+		return authorBookName;
+	}
+
+	public void setAuthorBookName(String authorBookName) {
+		this.authorBookName = authorBookName;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	@Override
+	public String toString() {
+		return "Author " +
+				" \nauthorName=" + getAuthorName() +
+				" \nauthorSurname=" + getAuthorSurname() +
+				" \nauthorBookName=" + getAuthorBookName() +
+				" \nisbn=" + getBook().getBookISBN();
+	}
+
+}
---
+++ 028_ConfigXML-SpEL/src/main/java/com/gokhangunay/Book.java
@@ -0,0 +1,33 @@
+package com.gokhangunay;
+
+/**
+ * @author gokhan.gunay
+ *
+ */
+public class Book {
+	
+	private String bookName;
+	
+	private Long bookISBN;
+	
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public Long getBookISBN() {
+		return bookISBN;
+	}
+
+	public void setBookISBN(Long bookISBN) {
+		this.bookISBN = bookISBN;
+	}
+
+
+	
+	
+
+}
